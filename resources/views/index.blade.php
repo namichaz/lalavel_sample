6f9f2103677439ae5c43abf7bb0cca9478a2c991
Tatsuya Namikwa <m757e888a7n7r@gmaiil.com>
2023-08-30 22:43:20 +0900

    Day2 lalavel practice add logo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,22 +1,34 @@
 <!DOCTYPE html>
-<html>
+<html lang="ja">
     <head>
+        <meta charset="utf-8">
+        <meta name="description" content="PHPのportfolioサイト">
         <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-
-        <title>我が家の愛犬ポチの日記</title>
+        <title>TatsuyaNamikawa</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,700;1,400&display=swap" rel="stylesheet">
     </head>
-
     <body>
-        <h1>我が家の愛犬ポチの日記</h1>
+        <header>
+            <h1><a href="" class="logo" src="{{ asset('images/icon.svg') }}" alt="portfolio"></a></h1>
+            <nav>
+                <ul class="main-nav">
+                    <li><a href="{({url'/')}}">Home</li>
+                    <li><a href="">Bio</li>
+                    <li><a href="">Skill</li>
+                    <li><a href="">Hobby</li>
+                </ul>
+            </nav>
+        </header>
 
-        <img src="{{ asset('images/inu.png.webp') }}" alt="dog">
+        {{-- <img src="{{ asset('images/inu.png.webp') }}" alt="dog"> --}}
 
-        <h2>うちの可愛い愛犬ポチを紹介するホームページです。</h2>
 
-        <p><a href="{{url('/welcome')}}">可愛いわんちゃんがうちに来てくれました</a></p>
-        <p><a href="{{url('/like')}}">ポチの好きな食べ物</a></p>
-        <p><a href="{{url('/dislike')}}">ポチの嫌いな食べ物</a></p>
-        <p><a href="{{url('/birthday')}}">ポチが１才になりました</a></p>
+        <p><a href="{({url'/welcome')}}">History</a></p>
+        <p><a href="{{url('/like')}}">Hobby</a></p>
+        <p><a href="{{url('/dislike')}}">Skills</a></p>
+        <p><a href="{{url('/birthday')}}">Contact</a></p>
     </body>
 
 </html>
